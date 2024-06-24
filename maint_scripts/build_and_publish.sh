9a6851594cfbc154c4175c4145cf68b912e3d7e9
Andy Smith <andy@strugglers.net>
2024-06-24 23:43:12 +0100

    Check for updates things in the themes
    We're overriding some files from the theme, so check if they have been
    updated recently so we can fix up our overrides.

--- maint_scripts/build_and_publish.sh
+++ maint_scripts/build_and_publish.sh
@@ -4,6 +4,14 @@ set -eu
 
 cd ~/src/ongoing-struggle
 
+# Check if things we override in the tabi theme have been updated at all.
+for f in templates/page.html; do
+    if [[ "themes/tabi/$f" -nt "$f" ]]; then
+        echo "$f in theme has been updated!"
+        exit 1
+    fi
+done
+
 zola build && \
     rsync -e 'ssh -q' -SHav --delete --exclude w/ \
     /home/andy/src/ongoing-struggle/public/ \
